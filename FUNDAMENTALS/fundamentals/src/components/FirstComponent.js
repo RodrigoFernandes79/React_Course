9e3e9a0d531948c030cdb5be1d997f674dec1c71
Rodrigo Fernandes <rodrigohf79@hotmail.com>
2023-12-10 11:03:36 -0300

    JSX Syntax

--- FUNDAMENTALS/fundamentals/src/components/FirstComponent.js
+++ FUNDAMENTALS/fundamentals/src/components/FirstComponent.js
@@ -1,8 +1,10 @@
 const FirstComponent = () => {
-
-    return (
-     <h1>My First Component</h1> 
-    )
-}
+  return (
+    <dir className="FirstComponent">
+      <h1>My First Component</h1>
+      <p>Jsx lesson</p>
+    </dir>
+  );
+};
 
 export default FirstComponent;
