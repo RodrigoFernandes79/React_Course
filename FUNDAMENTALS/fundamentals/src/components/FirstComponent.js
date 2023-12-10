c2cccd2e2431e6632f491d7f227b4c9f0ab39a18
Rodrigo Fernandes <rodrigohf79@hotmail.com>
2023-12-10 11:19:01 -0300

    Comments in React

--- FUNDAMENTALS/fundamentals/src/components/FirstComponent.js
+++ FUNDAMENTALS/fundamentals/src/components/FirstComponent.js
@@ -1,6 +1,15 @@
+//comentario fora do JSP
 const FirstComponent = () => {
+  //comentario pode ser dentro da funcao
+
+  /*
+comentario fora do JSP tambem pode ser
+em multiplas 
+linhas 
+*/
   return (
     <dir className="FirstComponent">
+      {/*Comentario no JSP dentro do return e dentro da tag pai */}
       <h1>My First Component</h1>
       <p>Jsx lesson</p>
     </dir>
