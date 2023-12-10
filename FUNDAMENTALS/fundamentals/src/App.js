b04076837f946e8f8a2a77b8c9fc85fe8d27b5ac
Rodrigo Fernandes <rodrigohf79@hotmail.com>
2023-12-10 12:03:51 -0300

    Inperpolation with Template Expression

--- FUNDAMENTALS/fundamentals/src/App.js
+++ FUNDAMENTALS/fundamentals/src/App.js
@@ -1,14 +1,16 @@
 //components:
-import FirstComponent from './components/FirstComponent';
+import FirstComponent from "./components/FirstComponent";
+import TemplateExpression from "./components/TemplateExpression";
 
 //CSS
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>React Components</h1>
       <FirstComponent />
+      <TemplateExpression />
     </div>
   );
 }
