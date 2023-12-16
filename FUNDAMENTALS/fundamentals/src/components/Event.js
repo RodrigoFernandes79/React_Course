8ef3ca311a43c58e705af8e828bfce33c371eb1c
Rodrigo Fernandes <rodrigohf79@hotmail.com>
2023-12-15 23:41:43 -0300

    Render Functions

--- FUNDAMENTALS/fundamentals/src/components/Event.js
+++ FUNDAMENTALS/fundamentals/src/components/Event.js
@@ -4,16 +4,31 @@ const Event = () => {
     console.log(e);
     console.log("Evento disparado");
   };
+
+  //Criando uma funcao para renderizacao de JSX(HTML):
+  const renderSomething = (boolean) => {
+    if (boolean) {
+      return <h1>Essa funcao renderiza JSX!</h1>;
+    } else {
+      return <h1>Se der false, a funcao renderiza esse JSX!</h1>;
+    }
+  };
   return (
     <div>
       <div>
         <button onClick={handleMyEventButton}>Click on me!</button>
       </div>
       <div>
-        {/*Criando uma funcao inline dentro do proprio evento */}
+        {/*Criando uma funcao inline dentro do proprio evento, podemos fazer isso quando for uma funcao simples:*/}
         <button onClick={() => console.log("Clicou!")}>
           Clique aqui tambem!
         </button>
+      </div>
+      <div>
+        {/*ao retornar true, ele chama o primeiro h1*/}
+        {renderSomething(true)}
+         {/*ao retornar false, ele chama o segundo  h1*/}
+        {renderSomething(false)}
       </div>
     </div>
   );
