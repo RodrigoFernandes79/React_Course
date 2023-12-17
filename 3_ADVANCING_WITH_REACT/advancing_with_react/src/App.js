4b0721863d5324df3ee2a8a6dc428df318a67cf8
Rodrigo Fernandes <rodrigohf79@hotmail.com>
2023-12-16 22:34:24 -0300

    Images in React

--- 3_ADVANCING_WITH_REACT/advancing_with_react/src/App.js
+++ 3_ADVANCING_WITH_REACT/advancing_with_react/src/App.js
@@ -1,11 +1,18 @@
-
-import './App.css';
+import "./App.css";
+import City from "./assets/city.jpg";
 
 function App() {
   return (
     <div className="App">
       <h1>SECAO 3</h1>
-      
+      <div>
+        {/*Publicando imagens da pasta public. o caminho da imagem eh /img1.jpg*/}
+        <img src="/img1.jpg" alt="Paisagem.jpg" />
+      </div>
+      <div>
+        {/*Publicando imagem da pasta assets, criamos uma pasta chamada assets dentro de src e demos um import City from "x./assets/city.jpg" */}
+        <img src={City} alt="Cidade.jpg" />
+      </div>
     </div>
   );
 }
